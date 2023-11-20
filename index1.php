a7d2f3c7efd1fba6db685030eb83efdf227f1e6f
Marcelo <0100mafc@gmail.com>
2023-11-20 13:51:49 -0300

    Primera Tarea

---
+++ index1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola</title>
+</head>
+<body>
+    <h1>Hola Mundo</h1>
+    <?php
+    $txt="hola";
+    $txt2="hola";
+    echo $txt."".$txt2."<br>";
+    echo "$txt $txt2 <br>";
+
+    $dia=11;
+    $fecha="Hoy es $dia de octubre";
+    echo $fecha
+    ?>
+
+</body>
+</html>
